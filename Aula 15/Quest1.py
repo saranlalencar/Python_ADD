fef2fe4f17c0001b3db10107c2163d82da071f80
saranlalencar <saranlac1265@gmail.com>
2026-01-22 21:50:52 -0300

    Atividades

---
+++ Aula 15/Quest1.py
@@ -0,0 +1,12 @@
+# 2. Crie um dicionário que organiza os contatos telefônicos de alguém. 
+# Como chave você deverá utilizar o nome da pessoa e como valor, o telefone da pessoa. 
+# Crie 5 contatos e exiba somente o telefone de um contato chamado "Mario".
+agenda = {
+    "Sara": "(85)99933-0000",
+    "Letícia": "(85)99999-9999",
+    "Mario": "(85)99966-1133",
+    "Theo": "(85)99955-7745",
+    "Thomas": "(85)99977-4466"
+}
+
+print(f"Telefone do Mario: {agenda['Mario']}")
